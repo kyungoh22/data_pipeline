0f1228a07a5b7801ab521dbe911687c23e283613
kyungoh22 <kh.oh@outlook.com>
2022-10-25 11:59:17 +0200

    using streamlit secrets

--- streamlit_deploy/plot_data.py
+++ streamlit_deploy/plot_data.py
@@ -5,35 +5,12 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-print (st.secrets["gcp_service_account"])
 
-# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-# SAMPLE_SPREADSHEET_ID = '1YBDYsqbOqbs94K1inMrL6ppFcjH-qbtYPE_mCXxCXzA'      # Get this from the url // Refer to google API documentation
-# SAMPLE_RANGE_NAME = 'A1:AA1000'
-
-
-# def read_data():
-
-#     credentials = service_account.Credentials.from_service_account_file( 
-#             "service_account.json", scopes=SCOPES)                            
-#     service = discovery.build('sheets', 'v4', credentials=credentials,cache_discovery=False)
-
-#     result = service.spreadsheets().values().get(
-#                 spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
-#     result = result['values']
-#     col_1 = result[0]
-#     col_2 = result[1]
-#     df = pd.DataFrame({'date': col_1, 'closing_price': col_2})
-#     # The data for dates before 24 Aug 2022 seems to be corrupt
-#     # So select only the dates after that.
-#     df['date'] = pd.to_datetime(df['date'])
-#     df = df [df['date'] > '2022-08-24']
-
-#     return df
+credentials_dict = st.secrets
 
 def read_data():
-    gc = gspread.service_account(filename="service_account.json")
-    
+    #gc = gspread.service_account(filename="service_account.json")
+    gc = gspread.service_account_from_dict(credentials_dict)
     sh = gc.open("free-data-pipeline").get_worksheet(1) # index 0 = sheet1, index 1 = sheet2, etc.
 
     col_1 = sh.col_values(1)
